64136bff033a3f1f5cbaef4deb7544b138867ff1
Moritz 'e1mo' Fromm <git@e1mo.de>
2023-05-25 13:40:35 +0200

    nix flake update: nixpkgs: 22.11 -> 23.05

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     sops-nix = {
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";

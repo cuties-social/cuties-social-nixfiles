c82b6d446c2444cb3aae2793a3fbc28473032af2
Isa <hi@f2k1.de>
2023-12-15 11:06:07 +0100

    update nixos to 23.11 and thus mastodon to 4.2.3

--- packages/default.nix
+++ packages/default.nix
@@ -1,22 +1,11 @@
 final: prev:
 rec {
-  patchedMastodonSource = final.applyPatches {
-    inherit (prev.mastodon.src) src;
-    /* src = prev.fetchFromGitHub {
-      owner = "mastodon";
-      repo = "mastodon";
-      rev = "v${prev.mastodon.version}";
-      sha256 = "sha256-/lyniar3TaQiDTaSKhfI6AF80sjo57VCjTydjv3T6kA=";
-    }; */
+  mastodon = prev.mastodon.override {
+    pname = "mastodon-cuties-socal";
     patches = [
       ./mastodon/allpatches.patch
       ./mastodon/troet.patch
     ];
-  };
-
-  mastodon = prev.mastodon.override {
-    pname = "mastodon-cuties-socal";
-    srcOverride = patchedMastodonSource;
   };
 
   customEmojis = prev.stdenv.mkDerivation {

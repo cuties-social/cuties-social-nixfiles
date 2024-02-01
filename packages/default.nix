09e114111efc18db58e10fea9f25fc3d389dd7a2
e1mo <git@e1mo.de>
2024-02-01 16:46:31 +0100

    mastodon: 4.2.4 -> 4.2.5
    To be removed once https://github.com/NixOS/nixpkgs/pull/285565 reaches
    the release channel

--- packages/default.nix
+++ packages/default.nix
@@ -2,9 +2,14 @@ inputs: final: prev:
 rec {
   mastodon = prev.mastodon.override {
     pname = "mastodon-cuties-socal";
+    version = "4.2.5";
     patches = [
       ./mastodon/allpatches.patch
       ./mastodon/troet.patch
+      (final.fetchpatch {
+        url = "https://github.com/mastodon/mastodon/compare/v4.2.4...v4.2.5.patch";
+        hash = "sha256-CtzYV1i34s33lV/1jeNcr9p/x4Es1zRaf4l1sNWVKYk=";
+      })
     ];
   };
 

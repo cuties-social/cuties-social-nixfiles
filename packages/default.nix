31fb17f439c9a84b5a6c9047b295fc706d7c278c
Isa <hi@f2k1.de>
2024-02-26 11:07:32 +0100

    mastodon: 4.2.7 -> 4.2.8

--- packages/default.nix
+++ packages/default.nix
@@ -2,18 +2,10 @@ inputs: final: prev:
 rec {
   mastodon = prev.mastodon.override {
     pname = "mastodon-cuties-socal";
-    version = "4.2.7";
-    gemset = builtins.toString (final.fetchurl {
-      url = "https://raw.githubusercontent.com/NixOS/nixpkgs/61acce0cb596050f5fa1c6ebf3f339a893361028/pkgs/servers/mastodon/gemset.nix";
-      hash = "sha256-Npny6jwon/xdTMU7xOZSZmiwId5IMDUgno1dG1FGkhA=";
-    });
+    version = "4.2.8";
     patches = [
       ./mastodon/allpatches.patch
       ./mastodon/troet.patch
-      (final.fetchpatch {
-        url = "https://github.com/mastodon/mastodon/compare/v4.2.6...v4.2.7.patch";
-        hash = "sha256-8FhlSIHOKIEjq62+rp8QdHY87qMCtDZwjyR0HabdHig=";
-      })
     ];
   };
 

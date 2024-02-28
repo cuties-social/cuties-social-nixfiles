af6a615fd5bf3bbb651964175cf6c74a371f5405
Isabelle <hi@f2k1.de>
2024-02-28 20:30:01 +0100

    Update packages/default.nix
    Co-authored-by: Nina <git@e1mo.de>

--- packages/default.nix
+++ packages/default.nix
@@ -2,7 +2,6 @@ inputs: final: prev:
 rec {
   mastodon = prev.mastodon.override {
     pname = "mastodon-cuties-socal";
-    version = "4.2.8";
     patches = [
       ./mastodon/allpatches.patch
       ./mastodon/troet.patch
